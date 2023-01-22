a07ae782d928b262918aa4617eedbc421d782e1b
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2023-01-22 22:52:09 +0000

    add comments

--- game.py
+++ game.py
@@ -1,11 +1,17 @@
 def game(player, computer, choices):
+    #iteration going through based on what the player has choosen
+    #if elif statements for the different options the player might of choosen and displays the results
+  
     while player not in choices:
       player = input("rock, paper, scissors?: ").lower()
-    
+      #ask for the players input
     if player == computer:
       print("computer :", computer)
+      #computers option is printed
       print("player :", player)
+      #players option is printed
       print("Tie!")
+      #displays result
     elif player == "rock":
       if computer == "paper":
         print("computer :", computer)
--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import random
 from game import *
 
 choices = ["rock","paper","scissors"]
+#list of options the computer and player could choose from
 computer =  random.choice(choices)
+#from the list of choices, the computer picks one option randomly
 player = None
 
 
 game(player, computer, choices)
+#call1 to begin the game
 
-#wont ask again after once
 while True:
+#loop each time the player finishes a game
   play_again = input("Play again? (yes/no): ").lower()
+#player is asked to play again
   if play_again != "yes":
     print("Bye!")
     break
+#ends game
   else:
     game(player, computer, choices)
+#call2 to begin game again
